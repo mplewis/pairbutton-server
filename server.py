fb828164fd8ce2b5951171b25658cd5a21807e7d
Matthew Lewis <matt@mplewis.com>
2014-11-24 01:47:30 -0600

    Lots of progress on signing, methods, auth

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, jsonify
 from flask.ext.restful import Resource, Api, reqparse
 from flask.ext.sqlalchemy import SQLAlchemy
+from Crypto.Hash import HMAC, SHA256
 
 import random
 import hashlib
+
+
+AUTH_KEY = 'Auth-Key'
+AUTH_SIGN = 'Auth-Signature'
 
 
 app = Flask(__name__)
@@ -34,7 +39,7 @@ def crypto_key_hex(bits=256):
 
 
 def make_error(message, code):
-    response = jsonify({'error': message})
+    response = jsonify({'message': message})
     response.status_code = code
     return response
 
@@ -48,12 +53,56 @@ def channel_with_id(channel_id):
         return channel, None
 
 
+def file_with_id(channel_id, file_id):
+    channel = Channel.query.filter_by(id=channel_id).first()
+    if not channel:
+        error = make_error('No channel with id {}'.format(channel_id), 404)
+        return None, error
+    file = (File.query.filter_by(id=file_id)
+            .filter_by(channel_id=channel_id).first())
+    if not file:
+        error = make_error('No file with id {}'.format(file_id), 404)
+        return None, error
+    else:
+        return file, None
+
+
 # http://stackoverflow.com/a/1960546/254187
 def serialize_sqla(sqla):
     d = {}
     for column in sqla.__table__.columns:
         d[column.name] = str(getattr(sqla, column.name))
     return d
+
+
+# https://gist.github.com/theY4Kman/3893296
+def hmac_sha256(key, data):
+    hash_obj = HMAC.new(key, data, SHA256)
+    return hash_obj.hexdigest()
+
+
+def authorized(header_name, expected):
+    if header_name in request.headers:
+        if request.headers[header_name] == expected:
+            return True
+    return False
+
+
+createFileParser = reqparse.RequestParser()
+createFileParser.add_argument(
+    'name', type=str, required=True, location='json',
+    help='name: File name is required')
+createFileParser.add_argument(
+    'data', type=str, required=True, location='json',
+    help='data: Initial file data is required')
+
+updateFileParser = reqparse.RequestParser()
+updateFileParser.add_argument(
+    'file_delta', type=str, required=True, location='json',
+    help='file_delta: File diff delta is required')
+updateFileParser.add_argument(
+    'expected_hash', type=str, required=True, location='json',
+    help='expected_hash: Hash of expected post-delta data is required')
 
 
 class Channel(db.Model):
@@ -103,16 +152,9 @@ class ChannelsEndpoint(Resource):
 class ChannelEndpoint(Resource):
     def get(self, channel_id):
         channel = Channel.query.filter_by(id=channel_id).first()
+        if not authorized(AUTH_KEY, channel.key):
+            return make_error('Unauthorized', 403)
         return jsonify(serialize_sqla(channel))
-
-
-newFileParser = reqparse.RequestParser()
-newFileParser.add_argument('name', type=str,
-                           help='name: File name is required',
-                           required=True, location='json')
-newFileParser.add_argument('data', type=str,
-                           help='data: Initial file data is required',
-                           required=True, location='json')
 
 
 class ChannelFilesEndpoint(Resource):
@@ -125,10 +167,14 @@ class ChannelFilesEndpoint(Resource):
 
     def post(self, channel_id):
         channel, error = channel_with_id(channel_id)
-        print(channel)
         if error:
             return error
-        file_data = newFileParser.parse_args()
+
+        expected = hmac_sha256(channel.key.encode(), request.data)
+        if not authorized(AUTH_SIGN, expected):
+            return make_error('Unauthorized', 403)
+
+        file_data = createFileParser.parse_args()
         file_data['channel_id'] = int(channel.id)
         f = File(**file_data)
         db.session.add(f)
@@ -136,9 +182,47 @@ class ChannelFilesEndpoint(Resource):
         return jsonify(serialize_sqla(f))
 
 
+class ChannelFileEndpoint(Resource):
+    def put(self, channel_id, file_id):
+        channel, error = channel_with_id(channel_id)
+        if error:
+            return error
+
+        expected = hmac_sha256(channel.key.encode(), request.data)
+        if not authorized(AUTH_SIGN, expected):
+            print(expected)
+            return make_error('Unauthorized', 403)
+
+        file, error = file_with_id(channel_id, file_id)
+        if error:
+            return error
+
+        change_data = updateFileParser.parse_args()
+        return jsonify(change_data)
+
+    def delete(self, channel_id, file_id):
+        channel, error = channel_with_id(channel_id)
+        if error:
+            return error
+
+        expected = hmac_sha256(channel.key.encode(), request.data)
+        if not authorized(AUTH_SIGN, expected):
+            return make_error('Unauthorized', 403)
+
+        file, error = file_with_id(channel_id, file_id)
+        if error:
+            return error
+
+        db.session.delete(file)
+        db.session.commit()
+        return '', 204
+
+
 api.add_resource(ChannelsEndpoint, '/channel')
 api.add_resource(ChannelEndpoint, '/channel/<string:channel_id>')
 api.add_resource(ChannelFilesEndpoint, '/channel/<string:channel_id>/file')
+api.add_resource(ChannelFileEndpoint,
+                 '/channel/<string:channel_id>/file/<string:file_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
