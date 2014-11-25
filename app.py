28b0aef282b53731e8af5d6b3d238f31daf6c54d
Matthew Lewis <matt@mplewis.com>
2014-11-25 03:54:18 -0600

    Add flask-admin for dev servers

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from models import db
+from models import db, Channel, File
 
 from flask import Flask
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
 
 
 def create_app(config_object):
@@ -19,6 +21,10 @@ def register_extensions(app):
     db.init_app(app)
     with app.app_context():
         db.create_all()
+    if app.debug:
+        admin = Admin(app)
+        admin.add_view(ModelView(Channel, db.session))
+        admin.add_view(ModelView(File, db.session))
 
 
 if __name__ == '__main__':
